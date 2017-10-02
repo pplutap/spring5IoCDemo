9baa578d1d43f7182a2eebe9fff88af741147f17
Pawel Pluta <pawel.pluta@eo.pl>
2017-10-02 21:52:06 +0200

    Get bean from application context

--- src/main/java/pl/pawel/DiDemoApplication.java
+++ src/main/java/pl/pawel/DiDemoApplication.java
@@ -2,11 +2,16 @@ package pl.pawel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import pl.pawel.controllers.MyController;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiDemoApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+		myController.hello();
 	}
 }
---
+++ src/main/java/pl/pawel/controllers/MyController.java
@@ -0,0 +1,11 @@
+package pl.pawel.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+	public void hello() {
+		System.out.println("Print hello");
+	}
+}
